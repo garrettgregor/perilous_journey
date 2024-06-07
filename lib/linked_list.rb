a9be03491efe0fd2deb655f15744ab210f060e99
Garrett Gregor <garrett.gregor@gmail.com>
2024-06-06 23:20:25 -0600

    feat: #count, #to_string and #append

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,4 +1,4 @@
-require 'node'
+require "node"
 
 class LinkedList
   attr_reader :head
@@ -8,6 +8,41 @@ class LinkedList
   end
 
   def append(surname)
-    @head = Node.new(surname)
+    if @head.nil?
+      @head = Node.new(surname)
+    elsif @head.next_node.nil?
+      @head.next_node = Node.new(surname)
+    else
+      @head = @head.next_node until @head.next_node.tail?
+      @head = Node.new(surname)
+    end
+  end
+
+  def count
+    if @head.nil?
+      0
+    elsif @head.next_node.nil?
+      1
+    else
+      count = 1
+      current_node = @head
+
+      until @head.next_node.nil?
+        @head = current_node.next_node
+
+        current_node = current_node.next_node
+        count += 1
+      end
+
+      count
+    end
+  end
+
+  def to_string
+    if @head.nil?
+      "No families"
+    elsif @head.next_node.nil?
+      "The #{@head.surname} family"
+    end
   end
 end
