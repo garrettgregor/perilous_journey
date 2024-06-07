a39e9cbff3bd2b2708ec602037714b2b543e2f96
Garrett Gregor <garrett.gregor@gmail.com>
2024-06-06 23:20:34 -0600

    test: #count, #to_string and #append

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe LinkedList do
       expect(list.head.next_node).to eq(nil)
     end
   end
+
+  describe "#count" do
+    it "adds a node to the head" do
+      expect(list.count).to eq(0)
+
+      list.append("West")
+      expect(list.count).to eq(1)
+      list.append("East")
+      expect(list.count).to eq(2)
+    end
+  end
+
+  describe "#to_string" do
+    it "generates a string of all the families in the list" do
+      expect(list.to_string).to eq("No families")
+
+      list.append("West")
+      expect(list.to_string).to eq("The West family")
+    end
+  end
 end
