6df30a6f81df65976e214aabe6732defe5b478ed
Garrett Gregor <garrett.gregor@gmail.com>
2024-06-06 22:30:20 -0600

    test: #append

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -2,11 +2,20 @@ require "./lib/node"
 require "./lib/linked_list"
 
 RSpec.describe LinkedList do
+  let!(:list) { LinkedList.new }
+
   describe "#initialize" do
     it "exists and has a nil head" do
-      list = LinkedList.new
       expect(list).to be_a(LinkedList)
       expect(list.head).to eq(nil)
+    end
+  end
+
+  describe "#append" do
+    it "adds a node to the head" do
+      expect(list.append("West")).to be_a(Node)
+      expect(list.head.surname).to eq("West")
+      expect(list.head.next_node).to eq(nil)
     end
   end
 end
