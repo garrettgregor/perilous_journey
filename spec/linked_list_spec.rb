ec8464ea668b46c24a37938b1f01d4fd96edda9e
Garrett Gregor <garrett.gregor@gmail.com>
2024-06-08 14:22:27 -0600

    test: add cases for multiple nodes #to_string #append and #count

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -1,41 +1,46 @@
-require "./lib/node"
-require "./lib/linked_list"
+require './spec/spec_helper'
 
 RSpec.describe LinkedList do
   let!(:list) { LinkedList.new }
 
-  describe "#initialize" do
-    it "exists and has a nil head" do
+  describe '#initialize' do
+    it 'exists and has a nil head' do
       expect(list).to be_a(LinkedList)
       expect(list.head).to eq(nil)
     end
   end
 
-  describe "#append" do
-    it "adds a node to the head" do
-      expect(list.append("West")).to be_a(Node)
-      expect(list.head.surname).to eq("West")
+  describe '#append' do
+    it 'adds a node to the head' do
+      expect(list.append('West')).to be_a(Node)
+      expect(list.head.surname).to eq('West')
       expect(list.head.next_node).to eq(nil)
     end
   end
 
-  describe "#count" do
-    it "adds a node to the head" do
+  describe '#count' do
+    it 'adds a node to the head' do
       expect(list.count).to eq(0)
 
-      list.append("West")
+      list.append('West')
       expect(list.count).to eq(1)
-      list.append("East")
+      list.append('East')
       expect(list.count).to eq(2)
+      list.append('South')
+      expect(list.count).to eq(3)
     end
   end
 
-  describe "#to_string" do
-    it "generates a string of all the families in the list" do
-      expect(list.to_string).to eq("No families")
+  describe '#to_string' do
+    it 'generates a string of all the families in the list' do
+      expect(list.to_string).to eq('No families')
 
-      list.append("West")
-      expect(list.to_string).to eq("The West family")
+      list.append('West')
+      expect(list.to_string).to eq('The West family')
+      list.append('East')
+      expect(list.to_string).to eq('The West family, followed by the East family')
+      list.append('South')
+      expect(list.to_string).to eq('The West family, followed by the East family, followed by the South family')
     end
   end
 end
